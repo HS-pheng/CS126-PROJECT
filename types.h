23402fc7e6bf512751704aea3db77e9d92d97cfe
Sakal <samnang.sakal@gmail.com>
2022-08-16 10:51:33 +0700

    file format validation

--- types.h
+++ types.h
@@ -4,36 +4,36 @@
 #include <fstream>
 using namespace std;
 
-typedef uint8_t  BYTE;
+typedef uint8_t BYTE;
 typedef uint32_t DWORD;
-typedef int32_t  LONG;
+typedef int32_t LONG;
 typedef uint16_t WORD;
 
-// Define BITMAPFILEHEADER struct 
+// Define BITMAPFILEHEADER struct
 typedef struct
 {
-    WORD   bfType;
-    DWORD  bfSize;
-    WORD   bfReserved1;
-    WORD   bfReserved2;
-    DWORD  bfOffBits;
+    WORD bfType;
+    DWORD bfSize;
+    WORD bfReserved1;
+    WORD bfReserved2;
+    DWORD bfOffBits;
 } __attribute__((__packed__))
 BITMAPFILEHEADER;
 
 // Define BITMAPINFOHEADER struct
 typedef struct
 {
-    DWORD  biSize;
-    LONG   biWidth;
-    LONG   biHeight;
-    WORD   biPlanes;
-    WORD   biBitCount;
-    DWORD  biCompression;
-    DWORD  biSizeImage;
-    LONG   biXPelsPerMeter;
-    LONG   biYPelsPerMeter;
-    DWORD  biClrUsed;
-    DWORD  biClrImportant;
+    DWORD biSize;
+    LONG biWidth;
+    LONG biHeight;
+    WORD biPlanes;
+    WORD biBitCount;
+    DWORD biCompression;
+    DWORD biSizeImage;
+    LONG biXPelsPerMeter;
+    LONG biYPelsPerMeter;
+    DWORD biClrUsed;
+    DWORD biClrImportant;
 } __attribute__((__packed__))
 BITMAPINFOHEADER;
 
@@ -43,109 +43,151 @@ typedef struct
     BYTE rgbtBlue;
     BYTE rgbtGreen;
     BYTE rgbtRed;
-}
-RGBTRIPLE;
+} RGBTRIPLE;
 
-enum operationMode {encrypt, decrypt};
+enum operationMode
+{
+    encrypt,
+    decrypt
+};
 
 class BitMapPicture
 {
-    public:
-        unsigned int height;
-        unsigned int width;
-        string fileName;
-        vector <RGBTRIPLE> rawPixels;
-        BITMAPFILEHEADER bf;
-        BITMAPINFOHEADER bi;
-        unsigned int padding;
+public:
+    unsigned int height;
+    unsigned int width;
+    string fileName;
+    vector<RGBTRIPLE> rawPixels;
+    BITMAPFILEHEADER bf;
+    BITMAPINFOHEADER bi;
+    unsigned int padding;
 
-        BitMapPicture(string fileName)
+    BitMapPicture(string fileName)
+    {
+        this->fileName = fileName;
+    };
+
+    // TODO: read from bitmapfile picture in a linear fashion
+    void read()
+    {
+        fstream fin(fileName, ios::in);
+
+        fin.read((char *)&bf, sizeof(BITMAPFILEHEADER));
+        fin.read((char *)&bi, sizeof(BITMAPINFOHEADER));
+
+        this->height = bi.biHeight;
+        this->width = bi.biWidth;
+        if (!is24_bit_BMP(bf, bi))
         {
-            this -> fileName = fileName;
-        };
-        
-        // TODO: read from bitmapfile picture in a linear fashion
-        void read()
+            fin.close();
+            cout << "Unsupported file format.\n";
+        }
+        // Calculate extra padding
+        padding = (4 - ((width * 3) % 4)) % 4;
+        for (int y = 0; y < height; y++)
         {
-            fstream fin(fileName, ios::in);
-
-            fin.read((char *) &bf, sizeof(BITMAPFILEHEADER));
-            fin.read((char *) &bi, sizeof(BITMAPINFOHEADER));
-
-            this->height = bi.biHeight;
-            this->width = bi.biWidth;
-
-            // Calculate extra padding 
-            padding = (4 - ((width * 3) % 4)) % 4;
-            for (int y = 0; y < height; y++)
+            for (int x = 0; x < width; x++)
             {
-                for (int x = 0; x < width; x++)
-                {
-                    RGBTRIPLE tmp;
-                    fin.read((char *) &tmp, sizeof(RGBTRIPLE));
-                    rawPixels.push_back(tmp);
-                }
-                fin.seekg(padding, ios::cur);
+                RGBTRIPLE tmp;
+                fin.read((char *)&tmp, sizeof(RGBTRIPLE));
+                rawPixels.push_back(tmp);
+            }
+            fin.seekg(padding, ios::cur);
+        }
+    }
+    // TODO: write to bitmapfile picture
+    void write()
+    {
+        fstream fout(fileName, ios::out);
+
+        fout.write((char *)&bf, sizeof(BITMAPFILEHEADER));
+        fout.write((char *)&bi, sizeof(BITMAPINFOHEADER));
+
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                fout.write((char *)&rawPixels[(width * y) + x], sizeof(RGBTRIPLE));
+            }
+            for (int k = 0; k < padding; k++)
+            {
+                // Write null byte to complete padding
+                fout.put(0x00);
             }
         }
-        // TODO: write to bitmapfile picture
-        void write()
+        fout.close();
+    }
+
+    void generateRandomNumberArray(unsigned int R[])
+    {
+        unsigned int r = R[0];
+        for (unsigned int k = 1; k < 2 * width * height; k++)
         {
-            fstream fout(fileName, ios::out);
+            r = r ^ r << 13;
+            r = r ^ r >> 17;
+            r = r ^ r << 5;
+            R[k] = r;
+        }
+    }
 
-            fout.write((char *) &bf, sizeof(BITMAPFILEHEADER));
-            fout.write((char *) &bi, sizeof(BITMAPINFOHEADER));
+    void generateRandomPermutationArray(unsigned int P[], unsigned int R[])
+    {
+        unsigned int totalPixels = height * width;
+        for (int i = 0; i < totalPixels; i++)
+            P[i] = i;
 
-            for (int y = 0; y < height; y++)
+        for (int i = totalPixels - 1; i >= 1; i--)
+            swap(P[R[i] % i], P[i]);
+    }
+
+    void cipherBMP(unsigned int R0, unsigned int SV, operationMode mode)
+    {
+        unsigned int totalPixels = height * width;
+        unsigned int *R = new unsigned int[2 * totalPixels];
+        R[0] = R0;
+        generateRandomNumberArray(R);
+
+        unsigned int *P = new unsigned int[totalPixels];
+        generateRandomPermutationArray(P, R);
+
+        vector<RGBTRIPLE> tempPixels(rawPixels.begin(), rawPixels.end());
+
+        if (mode == operationMode::encrypt)
+        {
+            for (unsigned int i = 0; i < totalPixels; i++)
             {
-                for (int x = 0; x < width; x++)
-                {
-                    fout.write((char *) &rawPixels[(width * y) + x], sizeof(RGBTRIPLE));
-                }
-                for (int k = 0; k < padding; k++)
-                {
-                    // Write null byte to complete padding
-                    fout.put(0x00);
-                }
+                rawPixels[P[i]] = tempPixels[i];
             }
+            ofstream ED_validate("ED_validate.txt");
+            string encrypted = fileName + "Encrypted";
+            ED_validate << encrypted;
+            ED_validate.close();
         }
-
-        void generateRandomNumberArray(unsigned int R[])
+        else
         {
-            unsigned int r = R[0];
-            for (unsigned int k = 1; k < 2 * width * height; k++)
+            for (unsigned int i = 0; i < totalPixels; i++)
             {
-                r = r ^ r << 13;
-                r = r ^ r >> 17;
-                r = r ^ r << 5;
-                R[k] = r;
+                rawPixels[i] = tempPixels[P[i]];
             }
+            ofstream ED_validate("ED_validate.txt");
+            string decrypted = fileName + "Decrypted";
+            ED_validate << decrypted;
+            ED_validate.close();
         }
+        delete[] R;
+        delete[] P;
+    }
 
-        void generateRandomPermutationArray(unsigned int P[], unsigned int R[]) 
+    bool is24_bit_BMP(BITMAPFILEHEADER bf, BITMAPINFOHEADER bi)
+    {
+        if (bf.bfType != 0x4d42 || bf.bfOffBits != 54 || bi.biSize != 40 ||
+            bi.biBitCount != 24 || bi.biCompression != 0)
         {
-            unsigned int totalPixels = height * width;
-            for (int i = 0; i < totalPixels; i++) P[i] = i;
-            
-            for (int i = totalPixels - 1; i >= 1; i--) swap(P[R[i] % i], P[i]);
+            return false;
         }
-
-        void cipherBMP(unsigned int R0, unsigned int SV, operationMode mode)
+        else
         {
-            unsigned int totalPixels = height * width;
-            unsigned int *R = new unsigned int [2 * totalPixels];
-            R[0] = R0;
-            generateRandomNumberArray(R);
-
-            unsigned int *P = new unsigned int [totalPixels];
-            generateRandomPermutationArray(P, R);
-
-            vector <RGBTRIPLE> tempPixels (rawPixels.begin(), rawPixels.end());
-
-            if (mode == operationMode::encrypt) for (unsigned int i = 0; i < totalPixels; i++) rawPixels[P[i]] = tempPixels[i];
-            else for (unsigned int i = 0; i < totalPixels; i++) rawPixels[i] = tempPixels[P[i]]; 
-
-            delete[] R;
-            delete[] P;
+            return true;
         }
+    }
 };
