3d040273a2a5b782e8ff5d68204cb53ae7e17fa9
Sakal <samnang.sakal@gmail.com>
2022-08-16 10:50:31 +0700

    include some UI (little validation)

--- cipher.cpp
+++ cipher.cpp
@@ -1,7 +1,7 @@
 /*
 Group names:
 Description
-Compilation: g++ cipher.cpp -o cipher 
+Compilation: g++ cipher.cpp -o cipher
 Run guide: ./cipher -enc/-dec inFile.bmp [optional: outFile.bmp]
 */
 
@@ -12,27 +12,45 @@ Run guide: ./cipher -enc/-dec inFile.bmp [optional: outFile.bmp]
 
 using namespace std;
 
-int main(int argc, char **argv)
+int main()
 {
     // TODO: input processing and validation (+check file's encryption and decryption status)
+    char choice;
+    string FILENAME;
+    cout << "========= Ecryption & Decryption Program =========" << endl;
+    cout << "[1]. Encryption" << endl
+         << "[2]. Decryption" << endl
+         << "\tEnter your choice: ";
+    cin >> choice;
+    if (choice_validation(choice) == false)
+    {
+        cout << "Invalid input! Please enter the choice in NUMBER!" << endl;
+        main();
+    }
 
+    cout << "Enter file name (in the format of bmp): ";
+    getline(cin, FILENAME);
+    BitMapPicture BMP(FILENAME);
 
     // TODO: password confirmation (if password is not configured, prompt user for entering the password)
 
-
     // TODO: generate key
 
-
-    // TODO: read bitmap file picture
-
-
+    // TODO: read bitmap file picture + file format validation
+    BMP.read();
+    
     // TODO: encryption or decryption
-
+    switch (choice)
+    {
+    case '1':
+        BMP.cipherBMP(321, 123, operationMode::encrypt);
+        break;
+    case '2':
+        BMP.cipherBMP(321, 123, operationMode::decrypt);
+        break;
+    }
 
     // TODO: write the pixels back to output bmp file
-    BitMapPicture BMP("sample.bmp");
-    BMP.read();
-    // BMP.cipherBMP(321, 123, operationMode::encrypt);
-    // BMP.cipherBMP(321, 123, operationMode::decrypt);
     BMP.write();
+    
 }
